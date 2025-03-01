72638a4163d4ba965faeaa429c35ab23194f12c2
ambarg27 <146844240+ambarg27@users.noreply.github.com>
2025-03-02 00:00:24 +0530

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,9 @@
+# Define the string to check
+import sys
+input_string = sys.argv[1]
+
+# Check if the string is equal to "xyz"
+if input_string == "YXV0b21hdGlvbnVzZXIxOjBZMUFHSU5Ud0ZCQWZVZUJCa1dlWGltd08zQUNMT29VNkN1YXg0c0pGY3Z6TlExUE9P":
+    print("Secret working")
+else:
+    print("Error: Secrets not working'")
